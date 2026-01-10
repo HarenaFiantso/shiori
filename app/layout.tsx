75e9422e2e3379cdd96c0626f4669893c73d7bd6
Fiantso Harena <fiantsorav@gmail.com>
2026-01-10 11:32:13 +0300

    chore: project generated with next16

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+
+import type { Metadata } from 'next';
+import { Raleway } from 'next/font/google';
+
+import './globals.css';
+
+const raleway = Raleway({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: '栞 Shiori',
+  description: 'Your calm space for focused productivity',
+};
+
+export default function RootLayout({ children}: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${raleway.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-2xl font-semibold text-center">Shiori says hello World !</h1>
+    </main>
+  );
+}
